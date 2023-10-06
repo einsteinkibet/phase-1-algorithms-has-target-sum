f57168f23ecce636db62741bb78d4a0bda703627
einsteinkibet <einsteinkibet0110@gmail.com>
2023-10-06 15:17:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // Sort the array in ascending order
+  array.sort((a,b) => a - b);
+
+  //sue two pointers, left at the index 0 (start) and right at the end of array.
+  let left = 0;
+  let right = array.length - 1;
+
+  // Iterate until the pointers meet at the center
+  while (left < right) {
+    const sum = array[left] + array[right];
+    if (sum === target) {
+      return true;
+
+    } else if (sum === target) {
+      return true ;
+
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +33,22 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(array, target):
+  Sort the array in ascending order
+  Initialize left pointer to 0
+  Initialize right pointer to array.length - 1
+
+  while left < right:
+    Calculate the sum of array[left] and array[right]
+
+    if the sum is equal to  target:
+      Return true
+    else if the sum is less than target:
+      Move the left pointer to the right
+    else:
+      Move the right pointer to the left
+
+  Return false
 */
 
 /*
